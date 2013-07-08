7af73c46567df3a3c6f16c91db4c3c5c014cd94f
imsamurai <im.samuray@gmail.com>
2013-07-08 13:50:04 +0300

    refactoring, try to fix zombie processes

--- Console/Command/Task/TaskServerTask.php
+++ Console/Command/Task/TaskServerTask.php
@@ -1,14 +1,11 @@
 <?
-
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\ProcessUtils;
-
 /**
  * Author: imsamurai <im.samuray@gmail.com>
  * Date: 10.06.2013
  * Time: 17:52:43
  * Format: http://book.cakephp.org/2.0/en/console-and-shells.html#shell-tasks
  */
+App::uses('TaskRunner', 'Task.Lib/Task');
 
 /**
  * @package Task.Console.Command.Task
@@ -16,13 +13,6 @@ use Symfony\Component\Process\ProcessUtils;
 class TaskServerTask extends Shell {
 
 	public $uses = array('Task.TaskServer', 'Task.TaskClient');
-
-	/**
-	 * Process
-	 *
-	 * @var Symfony\Component\Process\Process
-	 */
-	protected $_Process = null;
 
 	/**
 	 * {@inheritdoc}
@@ -40,91 +30,16 @@ class TaskServerTask extends Shell {
 		}
 
 		$ProcessManager = new Spork\ProcessManager();
-		$ProcessManager->process($tasks, array($this, 'start'), new Spork\Batch\Strategy\ChunkStrategy(count($tasks)));
-		$timeout = max((array)Hash::extract($tasks, '{n}.timeout'));
-		if ($timeout) {
-			$timeout+=60;
-			sleep($timeout);
-			$ProcessManager->killAll(SIGKILL);
-		}
-
-	}
-
-	/**
-	 * Notify client about stopped task
-	 *
-	 * @param array $task
-	 */
-	public function stop(array $task) {
-		$task = array(
-			'code' => $this->_Process->getExitCode(),
-			'code_string' => $this->_Process->getExitCodeText(),
-			'stdout' => $this->_Process->getOutput(),
-			'stderr' => $this->_Process->getErrorOutput(),
-			'stopped' => $this->_getCurrentDateTime()
-				) + $task;
-		$this->TaskServer->stoped($task);
-		$this->out("Task #{$task['id']} stopped, code " . (string) $task['code']);
-		return $task;
-	}
-
-	/**
-	 * Notify client about started task and run this task
-	 *
-	 * @param array $task
-	 */
-	public function start(array $task) {
-		ConnectionManager::getDataSource($this->TaskServer->useDbConfig)->reconnect(array('persistent'=>false));
-		$this->out("Task #{$task['id']} started");
-		$task['started'] = $this->_getCurrentDateTime();
-		$this->TaskServer->started($task);
-		return $this->run($task);
-	}
-
-	/**
-	 * Runs task
-	 *
-	 * @param array $task
-	 */
-	public function run(array $task) {
-		$this->_Process = new Process($task['command'] . $this->_argsToString($task['arguments']), $task['path']);
-		$this->_Process->setTimeout($task['timeout']);
-		$this->_Process->run(function ($type, $buffer) {
-					if ('err' === $type) {
-						$this->err($buffer);
-					} else {
-						$this->out($buffer);
-					}
-				});
-		return $this->stop($task);
-	}
-
-	/**
-	 * Convert array of arguments into string
-	 *
-	 * @param array $arguments
-	 * @return string
-	 */
-	protected function _argsToString(array $arguments) {
-		$stringArguments = '';
-		foreach ($arguments as $name => $value) {
-			if (is_numeric($name)) {
-				$stringArguments.=' ' . $value;
-			} else {
-				$stringArguments.=' ' . $name . ' ' . ProcessUtils::escapeArgument($value);
-			}
-		}
-
-		return $stringArguments;
-	}
-
-	/**
-	 * Returns current date for DB
-	 *
-	 * @return string
-	 */
-	protected function _getCurrentDateTime() {
-		return (new DateTime('now'))->format('Y-m-d H:i:s');
+		$ProcessManager->process($tasks, function ($task) {
+					$TaskRunner = new TaskRunner($this->TaskServer, $this->TaskClient);
+					return $TaskRunner->start($task);
+				}, new Spork\Batch\Strategy\ChunkStrategy(count($tasks)));
+//		$timeout = max((array)Hash::extract($tasks, '{n}.timeout'));
+//		if ($timeout) {
+//			$timeout+=60;
+//			sleep($timeout);
+//			$ProcessManager->killAll(SIGKILL);
+//		}
 	}
 
 }
---
+++ Lib/Task/TaskRunner.php
@@ -0,0 +1,137 @@
+<?php
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessUtils;
+
+/**
+ * Author: imsamurai <im.samuray@gmail.com>
+ * Date: 08.07.2013
+ * Time: 13:23:01
+ *
+ */
+
+/**
+ * Task Runner
+ *
+ * @package Task.Lib.Task
+ */
+class TaskRunner extends Object {
+
+	/**
+	 * TaskServer Model
+	 *
+	 * @var TaskServer
+	 */
+	protected $_TaskServer = null;
+
+	/**
+	 * TaskClient Model
+	 *
+	 * @var TaskClient
+	 */
+	protected $_TaskClient = null;
+
+	/**
+	 * Process
+	 *
+	 * @var Symfony\Component\Process\Process
+	 */
+	protected $_Process = null;
+
+	/**
+	 * Shell object
+	 *
+	 * @var Shell
+	 */
+	protected $_Shell = null;
+
+	/**
+	 * Constructor
+	 *
+	 * @param TaskServer $TaskServer
+	 * @param TaskClient $TaskClient
+	 */
+	public function __construct(TaskServer $TaskServer, TaskClient $TaskClient) {
+		$this->_TaskServer = $TaskServer;
+		$this->_TaskClient = $TaskClient;
+		$this->_Shell = new Shell();
+	}
+
+	/**
+	 * Notify client about stopped task
+	 *
+	 * @param array $task
+	 */
+	public function stop(array $task) {
+		$task = array(
+			'code' => $this->_Process->getExitCode(),
+			'code_string' => $this->_Process->getExitCodeText(),
+			'stdout' => $this->_Process->getOutput(),
+			'stderr' => $this->_Process->getErrorOutput(),
+			'stopped' => $this->_getCurrentDateTime()
+				) + $task;
+		$this->_TaskServer->stoped($task);
+		$this->_Shell->out("Task #{$task['id']} stopped, code " . (string) $task['code']);
+		return $task;
+	}
+
+	/**
+	 * Notify client about started task and run this task
+	 *
+	 * @param array $task
+	 */
+	public function start(array $task) {
+		ConnectionManager::getDataSource($this->_TaskServer->useDbConfig)->reconnect(array('persistent' => false));
+		$this->_Shell->out("Task #{$task['id']} started");
+		$task['started'] = $this->_getCurrentDateTime();
+		$this->_TaskServer->started($task);
+		return $this->run($task);
+	}
+
+	/**
+	 * Runs task
+	 *
+	 * @param array $task
+	 */
+	public function run(array $task) {
+		$this->_Process = new Process($task['command'] . $this->_argsToString($task['arguments']), $task['path']);
+		$this->_Process->setTimeout($task['timeout']);
+		$this->_Process->run(function ($type, $buffer) {
+					if ('err' === $type) {
+						$this->_Shell->err($buffer);
+					} else {
+						$this->_Shell->out($buffer);
+					}
+				});
+		return $this->stop($task);
+	}
+
+	/**
+	 * Convert array of arguments into string
+	 *
+	 * @param array $arguments
+	 * @return string
+	 */
+	protected function _argsToString(array $arguments) {
+		$stringArguments = '';
+		foreach ($arguments as $name => $value) {
+			if (is_numeric($name)) {
+				$stringArguments.=' ' . $value;
+			} else {
+				$stringArguments.=' ' . $name . ' ' . ProcessUtils::escapeArgument($value);
+			}
+		}
+
+		return $stringArguments;
+	}
+
+	/**
+	 * Returns current date for DB
+	 *
+	 * @return string
+	 */
+	protected function _getCurrentDateTime() {
+		return (new DateTime('now'))->format('Y-m-d H:i:s');
+	}
+
+}
