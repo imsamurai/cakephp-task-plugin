57b1b1062377edda2343db753fe73f26e924b1da
imsamurai <imsamurai@imsamurai-2>
2014-01-18 01:02:55 +0200

    add travis and all test suite

---
+++ Test/Case/AllTaskTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Author: imsamurai <im.samuray@gmail.com>
+ * Date: 29.10.2013
+ * Time: 21:50:00
+ */
+class AllTaskTest extends PHPUnit_Framework_TestSuite {
+
+	/**
+	 * Suite define the tests for this suite
+	 *
+	 * @return void
+	 */
+	public static function suite() {
+		$suite = new CakeTestSuite('All Task Tests');
+
+		$path = App::pluginPath('Task') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+		return $suite;
+	}
+
+}
