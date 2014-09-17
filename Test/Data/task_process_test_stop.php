f12411bd14f130163f3387c7955d2df6c1003790
imsamurai <imsamurai@imsamurai-2>
2014-09-17 18:17:00 +0300

    fix phpdoc

--- Test/Data/task_process_test_stop.php
+++ Test/Data/task_process_test_stop.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Author: imsamurai <im.samuray@gmail.com>
- * Date: 09.09.2014
- * Time: 16:34:21
- * Format: http://book.cakephp.org/2.0/en/views.html
- */
-
 define('FILE_NAME', $argv[1]);
 file_put_contents(FILE_NAME, '');
 
